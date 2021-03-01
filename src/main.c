b03edb8f66f48643ab97b793345c6bd5e4a17540
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2021-02-28 21:41:37 -0500

    Add a simple CLI

--- src/main.c
+++ src/main.c
@@ -1,8 +1,38 @@
 #include "ui.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(void)
+#define VERSION "0.0.1"
+
+int main(int argc, char *argv[])
 {
+	const char *progname = argv[0] ? argv[0] : "(no-name)";
+	int i;
+	for (i = 1; i < argc; ++i) {
+		if (!strcmp("-h", argv[i])) {
+			printf(
+				"Usage: %s [{-h | -v}]\n"
+				"Plays the game Monster Money Maze.\n"
+				"\n"
+				"Options:\n"
+				"  -h Print this help rather than playing.\n"
+				"  -v Print the version rather than playing.\n"
+				"\n"
+				"See the in-game help information for more.\n",
+				progname);
+			return EXIT_SUCCESS;
+		} else if (!strcmp("-v", argv[i])) {
+			printf("%s version " VERSION "\n", progname);
+			return EXIT_SUCCESS;
+		} else {
+			fprintf(stderr, "%s: Invalid argument: %s\n",
+				progname, argv[i]);
+			return EXIT_FAILURE;
+		}
+	}
+
 	ui_run();
+
 	return EXIT_SUCCESS;
 }
