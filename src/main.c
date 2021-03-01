a01a9f0b139c63c1fb5de3c9fb9f4e19d7aa308a
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2021-03-01 13:10:05 -0500

    Improve CLI error messages

--- src/main.c
+++ src/main.c
@@ -4,14 +4,16 @@
 #include <string.h>
 
 /* The version of the program: */
-#define VERSION "0.2.0"
+#define VERSION "0.2.1"
 
 int main(int argc, char *argv[])
 {
 	const char *progname = argv[0] ? argv[0] : "(no-name)";
-	int i;
-	for (i = 1; i < argc; ++i) {
-		if (!strcmp("-h", argv[i])) {
+	if (argc > 2) {
+		fprintf(stderr, "%s: Too many arguments\n", progname);
+		return EXIT_FAILURE;
+	} else if (argc > 1) {
+		if (!strcmp("-h", argv[1])) {
 			printf(
 				"Usage: %s [{-h | -v}]\n"
 				"Plays the game Monster Money Maze.\n"
@@ -23,17 +25,15 @@ int main(int argc, char *argv[])
 				"See the in-game help information for more.\n",
 				progname);
 			return EXIT_SUCCESS;
-		} else if (!strcmp("-v", argv[i])) {
+		} else if (!strcmp("-v", argv[1])) {
 			printf("%s version " VERSION "\n", progname);
 			return EXIT_SUCCESS;
 		} else {
-			fprintf(stderr, "%s: Invalid argument: %s\n",
-				progname, argv[i]);
+			fprintf(stderr, "%s: Invalid argument\n", progname);
 			return EXIT_FAILURE;
 		}
+	} else {
+		ui_run();
+		return EXIT_SUCCESS;
 	}
-
-	ui_run();
-
-	return EXIT_SUCCESS;
 }
