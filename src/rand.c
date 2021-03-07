9aed766894d2f1ee08f82694e5fd1f9ca1d53ac5
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2021-03-07 14:09:14 -0500

    Remove bit shuffling in randomness
    The LCG seems to work about as well for the important usages.

--- src/rand.c
+++ src/rand.c
@@ -2,9 +2,7 @@
 
 RAND_TYPE rand_gen(RAND_TYPE *rand)
 {
-	/* Use and LCG. */
+	/* LCG: */
         *rand = (MAX_RAND & (1103515245UL * *rand)) + 12345;
-	/* Shuffle bits somewhat in case lower bits are biased. */
-	*rand = (MAX_RAND & (*rand << 4)) | ((0xF0000000UL & *rand) >> 28);
 	return *rand;
 }
--- src/version.h
+++ src/version.h
@@ -1,2 +1,2 @@
 /* The version of the program: */
-#define VERSION "0.5.4"
+#define VERSION "0.5.5"
